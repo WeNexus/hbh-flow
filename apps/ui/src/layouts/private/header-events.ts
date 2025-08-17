212bd7f83ec856636f9d6cd34ceed355d69898c7
WeNexusTeam <wenexusltd@gmail.com>
2025-08-17 15:26:47 +0600

    add layout level progressbar

--- apps/ui/src/layouts/private/header-events.ts
+++ apps/ui/src/layouts/private/header-events.ts
@@ -2,5 +2,6 @@ export const HeaderEvents = {
   query: 'flow:header-query',
   querySubmit: 'flow:header-query-submit',
   ui: 'flow:header-ui',
-}
-
+  loadingShow: 'flow:header-loading-show',
+  loadingHide: 'flow:header-loading-hide',
+};
--- apps/ui/src/layouts/private/header.tsx
+++ apps/ui/src/layouts/private/header.tsx
@@ -2,11 +2,13 @@ import ColorModeIconDropdown from '@/components/theme/color-mode-icon-dropdown.t
 import CustomDatePicker from '@/components/custom-date-picker.tsx';
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 import { HeaderEvents } from '@/layouts/private/header-events.ts';
+import LinearProgress from '@mui/material/LinearProgress';
 import InputAdornment from '@mui/material/InputAdornment';
 import HeaderBreadcrumbs from './header-breadcrumbs.tsx';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import { useSearchParams } from 'react-router';
 import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
 
 import {
   type KeyboardEvent,
@@ -21,6 +23,7 @@ export default function Header() {
   const [query, setQuery] = useState(searchParams.get('q') || '');
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [showSearch, setShowSearch] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const onQueryChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     window.dispatchEvent(
@@ -68,10 +71,20 @@ export default function Header() {
         }));
       }
     };
+    const loadingShowHandler = () => setLoading(true);
+    const loadingHideHandler = () => setLoading(false);
 
     window.addEventListener(HeaderEvents.querySubmit as any, queryClearHandler);
     window.addEventListener(HeaderEvents.query as any, queryHandler);
     window.addEventListener(HeaderEvents.ui as any, stateHandler);
+    window.addEventListener(
+      HeaderEvents.loadingShow as any,
+      loadingShowHandler,
+    );
+    window.addEventListener(
+      HeaderEvents.loadingHide as any,
+      loadingHideHandler,
+    );
 
     return () => {
       window.removeEventListener(
@@ -80,45 +93,58 @@ export default function Header() {
       );
       window.removeEventListener(HeaderEvents.query as any, queryHandler);
       window.removeEventListener(HeaderEvents.ui as any, stateHandler);
+      window.removeEventListener(
+        HeaderEvents.loadingShow as any,
+        loadingShowHandler,
+      );
+      window.removeEventListener(
+        HeaderEvents.loadingHide as any,
+        loadingHideHandler,
+      );
     };
   }, [setSearchParams]);
 
   return (
-    <Stack
-      direction="row"
-      sx={{
-        display: { xs: 'none', md: 'flex' },
-        width: '100%',
-        alignItems: { xs: 'flex-start', md: 'center' },
-        justifyContent: 'space-between',
-        maxWidth: { sm: '100%', md: '1700px' },
-        pt: 1.5,
-      }}
-      spacing={2}
-    >
-      <HeaderBreadcrumbs />
-      <Stack direction="row" sx={{ gap: 1 }}>
-        {showSearch && (
-          <OutlinedInput
-            onChange={onQueryChange}
-            onKeyDown={onEnter}
-            placeholder="Search..."
-            sx={{ flexGrow: 1 }}
-            value={query}
-            size="small"
-            startAdornment={
-              <InputAdornment position="start" sx={{ color: 'text.primary' }}>
-                <SearchRoundedIcon fontSize="small" />
-              </InputAdornment>
-            }
-            inputProps={{
-              'aria-label': 'search',
-            }}
-          />
-        )}
-        {showDatePicker && <CustomDatePicker />}
-        <ColorModeIconDropdown />
+    <Stack direction="column" sx={{ width: '100%' }}>
+      <Stack
+        direction="row"
+        sx={{
+          display: { xs: 'none', md: 'flex' },
+          width: '100%',
+          alignItems: { xs: 'flex-start', md: 'center' },
+          justifyContent: 'space-between',
+          maxWidth: { sm: '100%', md: '1700px' },
+          pt: 1.5,
+        }}
+        spacing={2}
+      >
+        <HeaderBreadcrumbs />
+        <Stack direction="row" sx={{ gap: 1 }}>
+          {showSearch && (
+            <OutlinedInput
+              onChange={onQueryChange}
+              onKeyDown={onEnter}
+              placeholder="Search..."
+              sx={{ flexGrow: 1 }}
+              value={query}
+              size="small"
+              startAdornment={
+                <InputAdornment position="start" sx={{ color: 'text.primary' }}>
+                  <SearchRoundedIcon fontSize="small" />
+                </InputAdornment>
+              }
+              inputProps={{
+                'aria-label': 'search',
+              }}
+            />
+          )}
+          {showDatePicker && <CustomDatePicker />}
+          <ColorModeIconDropdown />
+        </Stack>
       </Stack>
+      {loading ? (
+        <LinearProgress variant="indeterminate" sx={{ height: 2, mt: 1 }} />
+      ): <Box sx={{ height: 2, mt: 1 }}></Box>}
     </Stack>
   );
 }
