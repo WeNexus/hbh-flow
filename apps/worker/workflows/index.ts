1c11c4c0fd75ef2267bd7dfaa7584a00b331cc50
WeNexusTeam <wenexusltd@gmail.com>
2025-08-21 14:50:30 +0600

    remove TestWorkflow

--- apps/worker/workflows/index.ts
+++ apps/worker/workflows/index.ts
@@ -1,5 +1,4 @@
 import { WorkflowBase } from '#lib/workflow/misc/workflow-base.js';
-import { TestWorkflow } from './test.workflow.js';
 import { Type } from '@nestjs/common';
 
-export const workflows: Type<WorkflowBase>[] = [TestWorkflow];
+export const workflows: Type<WorkflowBase>[] = [];
