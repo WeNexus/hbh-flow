a8855bd07020580d823660ea61b50a6148d1a622
WeNexusTeam <wenexusltd@gmail.com>
2025-08-30 12:51:16 +0600

    add crm->bigcommerce flow to workflows array

--- apps/worker/workflows/index.ts
+++ apps/worker/workflows/index.ts
@@ -1,4 +1,7 @@
+import { PushCrmContactToBigcommerceWorkflow } from '#app/worker/workflows/push-crm-contact-to-bigcommerce.workflow';
 import { WorkflowBase } from '#lib/workflow/misc/workflow-base.js';
 import { Type } from '@nestjs/common';
 
-export const workflows: Type<WorkflowBase>[] = [];
+export const workflows: Type<WorkflowBase>[] = [
+  PushCrmContactToBigcommerceWorkflow,
+];
